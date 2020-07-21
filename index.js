0ce9c10ebc21028760094b93fcfb74c147fd0bda
Dan F-M <foreman.mackey@gmail.com>
2020-07-21 16:55:42 -0400

    sending post

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fetch = require("node-fetch");
 
 try {
   // `who-to-greet` input defined in action metadata file
   const webhookId = core.getInput("webhook_id");
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  const webhookToken = core.getInput("webhook_token");
+  const ref = github.context.payload.ref;
+
+  const url = `https://readthedocs.org/api/v2/webhook/${webhookId}/`;
+  const params = new URLSearchParams();
+  params.append("branches", "main");
+  params.append("token", webhookToken);
+
+  console.log(`The event ref: ${ref}`);
 } catch (error) {
   core.setFailed(error.message);
 }
