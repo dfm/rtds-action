394ec23d7e00dcd2b8ed138a6487912702d78587
Dan F-M <foreman.mackey@gmail.com>
2020-07-21 14:25:56 -0400

    initial

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+extensions = ["nbsphinx"]
+
+source_suffix = ".rst"
+master_doc = "index"
+
+project = "rtds-action"
+author = "Dan Foreman-Mackey"
+copyright = "2020, " + author
+version = "0.1.0"
+release = "0.1.0"
+
+exclude_patterns = ["_build"]
---
+++ docs/index.rst
@@ -0,0 +1,11 @@
+===========
+rtds-action
+===========
+
+This is just a demo.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Tutorials
+
+   tutorials/tutorial
